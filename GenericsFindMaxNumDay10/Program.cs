1938fbce0e2e41a42d33e4b60e23fec826e98e47
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-02 17:46:00 +0530

    Added Welcome To Find Maximum Number Using Generics

---
+++ GenericsFindMaxNumDay10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsFindMaxNumDay10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to find maximum number using Generics Program");
+        }
+    }
+}
