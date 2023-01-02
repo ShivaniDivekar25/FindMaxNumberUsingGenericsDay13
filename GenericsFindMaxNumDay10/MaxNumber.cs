e333636baf87e33c9331692496f300f699776e72
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-02 18:13:25 +0530

    Modified to Find Maximum For String Without Generics program

--- GenericsFindMaxNumDay10/MaxNumber.cs
+++ GenericsFindMaxNumDay10/MaxNumber.cs
@@ -8,7 +8,7 @@ namespace GenericsFindMaxNumDay10
 {
     internal class MaxNumber        //Creating class
     {
-        public static void maxNumber()          //Creating method to find maximum number using CompareTo method
+        public static void maxNumber()          //Creating method to find maximum integer number using CompareTo method
         {
             int a = 45;
             int b = 60;
@@ -26,11 +26,29 @@ namespace GenericsFindMaxNumDay10
                 Console.WriteLine("The maximum number is : " + c);
             }
         }
-        public static void maxNumber1()         //Creating method to find maximum number using CompareTo method
+        public static void maxNumber1()         //Creating method to find maximum float number using CompareTo method
         {
             float a = 11.34f;
             float b = 45.96f;
             float c = 75.74f;
+            if ((a.CompareTo(b) > 0) && (a.CompareTo(c) > 0))
+            {
+                Console.WriteLine("The maximum number is:" + a);
+            }
+            else if ((b.CompareTo(a) > 0) && (b.CompareTo(c) > 0))
+            {
+                Console.WriteLine("The maximum number is: " + b);
+            }
+            else
+            {
+                Console.WriteLine("The maximum number is : " + c);
+            }
+        }
+        public static void maxNumber2()         //Creating method to find maximum for string using ComapreTo method
+        {
+            string a = "Apple";
+            string b = "Banana";
+            string c = "Pear";
             if ((a.CompareTo(b) > 0) && (a.CompareTo(c) > 0))
             {
                 Console.WriteLine("The maximum number is:" + a);
--- GenericsFindMaxNumDay10/Program.cs
+++ GenericsFindMaxNumDay10/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to find maximum number using Generics Program");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:Find integer maximum number\n2:Find float maximum number");     
+            Console.WriteLine("1:Find integer maximum number\n2:Find float maximum number\n3:Find maximum for string");     
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)             //Calling different methods using switch case for UC1 and UC2
             {
@@ -15,6 +15,9 @@
                     break;
                 case 2:
                     MaxNumber.maxNumber1();         //calling method for float
+                    break;
+                case 3:
+                    MaxNumber.maxNumber2();         //Calling method for string
                     break;
                 default:
                     Console.WriteLine("Please enter correct option");
